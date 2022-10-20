e932f34652b0bab6645654ff170f9ad85c8e9ec4
Jeden1102 <bum123@o2.pl>
2022-10-20 16:27:22 +0200

    add

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,103 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest" target="_blank" rel="noopener">unit-jest</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <GMapAutocomplete
+       placeholder="This is a placeholder"
+       @place_changed="setPlace"
+    >
+  </GMapAutocomplete>
+  <GMapMap
+    :center="center"
+    :zoom="10"
+    map-type-id="terrain"
+    style="width: 100vw; height: 20rem"
+  >
+    <GMapCluster :zoomOnClick="true">
+      <GMapMarker
+        :key="index"
+        v-for="(m, index) in markers"
+        :position="m.position"
+        :clickable="true"
+        :draggable="false"
+        :icon="require('@/assets/logo.png')"
+        @click="handleClick"
+      >
+      <GMapInfoWindow>
+        <div>I am in info window <MyComponent/>
+        </div>
+      </GMapInfoWindow>
+    </GMapMarker>
+    </GMapCluster>
+  </GMapMap>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  data() {
+    return {
+      center: { lat: 51.093048, lng: 6.84212 },
+      markers: [
+        {
+          test:"abc",
+          position: {
+            lat: 57.093048,
+            lng: 6.84212,
+          },
+        },
+        {
+          position: {
+            lat: 51.198429,
+            lng: 6.69529,
+          },
+        },
+        {
+          position: {
+            lat: 51.165218,
+            lng: 7.067116,
+          },
+        },
+        {
+          position: {
+            lat: 51.09256,
+            lng: 6.84074,
+          },
+        },
+        {
+          position: {
+            lat: 51.09256,
+            lng: 6.84074,
+          },
+        },
+        {
+          position: {
+            lat: 53.09256,
+            lng: 6.84074,
+          },
+        },
+        {
+          test:"Abc",
+          position: {
+            lat: 55.09256,
+            lng: 6.84074,
+          },
+        },
+        {
+          position: {
+            lat: 51.09256,
+            lng: 6.84074,
+          },
+        },
+      ],
+    };
+  },
+  methods: {
+    handleClick(event){
+      console.log(event)
+    }
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style>
+body {
+  margin: 0;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import VueGoogleMaps from '@fawmi/vue-google-maps'
+const app = createApp(App);
+app.use(VueGoogleMaps, {
+    load: {
+        key: 'AIzaSyCMO7f1IdmC2znLs6fmVNgPwu98guyCWuY',
+        libraries: "places"
+    },
+}).mount('#app')
